ead0a3b9db990b93885fd34670f8c043362987df
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-06-23 13:09:41 +0530

    create signup route with bcrypt hashed password

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js"
-dotenv.config();
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
+dotenv.config();
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -14,9 +15,12 @@ mongoose
   });
 
 const app = express();
+// => allow the json data
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log(`Server is running on port 3000!!!`);
 });
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
