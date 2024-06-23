e7b340b5d9076e3e2455bc4e260f2cb51dd3ee7a
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-06-24 02:28:53 +0530

    proxy setting update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
     },
   },
 
