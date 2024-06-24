7c73197d763cce50c40bc6a327746ad78a16d86e
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-06-25 02:55:59 +0530

    added few comments

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -21,12 +21,27 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
+    // => check if user exist with the provided email in our db
     const validUser = await User.findOne({ email });
+
+    // => if the email is not valid return the error, errorHandler created in error.js
     if (!validUser) return next(errorHandler(404, "User not found"));
+
+    // => compare the password
     const validPassowrd = bcryptjs.compareSync(password, validUser.password);
+
+    // => if not matched return error
     if (!validPassowrd) return next(errorHandler(401, "wrong credentials"));
+
+    // => if both credentials are same as database, sign a token using jwt,
+    // => jwt takes user id and our secret token to salt the token
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const {password: pass, ...restData} = validUser._doc
+
+    // => take the all field except password
+    const { password: pass, ...restData } = validUser._doc;
+
+    // => return the response, set cookie name as access_token and allowed only http req,
+    // => set status to 200, response with the restData
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
