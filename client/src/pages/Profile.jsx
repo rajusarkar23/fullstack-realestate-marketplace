08e410ff9bce5299269b3b9f62a1208da4127971
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-07-13 15:30:56 +0530

    add delete listing functionalities

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -149,6 +149,25 @@ function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListing((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div
       className="p-3 max-w-lg mx-auto
@@ -262,9 +281,13 @@ function Profile() {
             </Link>
 
             <div className="flex flex-col">
-            <button className="text-red-700 font-bold uppercase">
+              <button
+                onClick={() => handleListingDelete(listing._id)}
+                className="text-red-700 font-bold uppercase"
+              >
                 Delete
-              </button> <button className="text-green-700 font-bold uppercase">
+              </button>{" "}
+              <button className="text-green-700 font-bold uppercase">
                 Edit
               </button>
             </div>
