9c206d1642572ef376c5421658b8e66d0e6c6048
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-07-16 17:07:16 +0530

    add search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -62,3 +62,62 @@ export const getListingById = async (req, res, next) => {
     next(error);
   }
 };
+// => getListings for search
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    let offer = req.query.offer;
+    // => if offer is undefined or false
+    // => offer = search the db, set offer to both false and true
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+    // => same logic as above in offer section
+    if (furnished === undefined || "furnished" === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    // => same logic as above in offer section
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    // => same logic as above in offer section
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    let searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      // => $regex = search the heading, $options:i = do not care about the lowercase and uppercase
+      name: { $regex: searchTerm, $options: "i" },
+      // => Search inside the name for:
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      // => sort in order as declare in above
+      .sort({
+        [sort]: order,
+      })
+      // => after the sort limit the search to limit
+      // => skip the start index
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
