cb29a1ec65b893026bdc377bcfd034498df1d6f0
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-03-23 21:58:07 +0530

    add cors

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 import path from "path"
+import cors from "cors"
 
 const PORT = process.env.PORT || 3030;
 
@@ -28,6 +29,8 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use(cors())
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
